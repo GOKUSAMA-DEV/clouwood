c6841c23f8f46370631d293cb07a417b281407ab
Gaurav <gouravsahu394@gmail.com>
2021-07-25 10:40:57 +0530

    Updates

--- static/js/main.fcb0b767.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcloud=this.webpackJsonpcloud||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);t(1);var c=t(3),r=t.n(c),o=(t(8),t(0)),s=function(){return Object(o.jsx)(o.Fragment,{children:Object(o.jsx)("h1",{children:"Helo world"})})};r.a.render(Object(o.jsx)(o.Fragment,{children:Object(o.jsx)(s,{})}),document.getElementById("root"))},8:function(e,n,t){}},[[10,1,2]]]);
-//# sourceMappingURL=main.fcb0b767.chunk.js.map
